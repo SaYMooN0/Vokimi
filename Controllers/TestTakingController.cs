c4422667d24ea2b625941ecd071a20dd318d29da
SaYMooN <shokoladnysaymoon@mail.ru>
2023-11-22 18:03:02 +0300

    started working on the test taking page

--- Controllers/TestTakingController.cs
+++ Controllers/TestTakingController.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Vokimi.Models.ViewModels.TestTaking;
 
 namespace Vokimi.Controllers
 {
     public class TestTakingController : Controller
     {
-        public IActionResult Test(int testId)
+        public IActionResult Test(int id)
         {
-            return View();
+            TestTakingViewModel vm = new();
+            return View(vm);
+        }
+        [HttpPost]
+        public IActionResult TestTaken()
+        {
+            TestTakingViewModel vm = new();
+            return Ok();
         }
     }
 }
---
+++ Models/ViewModels/TestTaking/TestTakingViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Vokimi.Models.DataBaseClasses;
+
+namespace Vokimi.Models.ViewModels.TestTaking
+{
+    public class TestTakingViewModel
+    {
+        public int TestId { get; set; }
+        public List<Question> Questions { get; set; } = new();
+
+    }
+}
