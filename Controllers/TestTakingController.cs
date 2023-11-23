7e57a1830d17ea7770735c374500518d712f6725
SaYMooN <shokoladnysaymoon@mail.ru>
2023-11-23 22:40:20 +0300

    Added the ability to answer questions and finish passing the test

--- Controllers/TestTakingController.cs
+++ Controllers/TestTakingController.cs
@@ -1,20 +1,41 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Vokimi.Models.DataBaseClasses;
 using Vokimi.Models.ViewModels.TestTaking;
+using VokimiServices;
 
 namespace Vokimi.Controllers
 {
     public class TestTakingController : Controller
     {
-        public IActionResult Test(int id)
+        private IDataBase _dataBase;
+        private VokimiServices.ILogger _logger;
+
+        public TestTakingController(IDataBase dataBase, VokimiServices.ILogger logger)
+        {
+            _dataBase = dataBase;
+            _logger = logger;
+        }
+        public async Task<IActionResult> Test(int id)
         {
             TestTakingViewModel vm = new();
+            Test t= await _dataBase.GetTestByIdAsync(id);
+            vm.TestId = t.Id;
+            vm.Questions = (await _dataBase.GetQuestionsForTestAsync(id)).ToList();
             return View(vm);
         }
         [HttpPost]
-        public IActionResult TestTaken()
+        public IActionResult TestTaken(List<int> answers, int testId)
         {
-            TestTakingViewModel vm = new();
-            return Ok();
+            int sum = 0;
+            foreach (int item in answers)
+                sum += item;
+            return RedirectToAction("Result", new Tuple<int, int>(sum, testId));
         }
+        [HttpGet]
+        public IActionResult Result(Tuple<int, int> tuple)
+        {
+            return View(tuple);
+        }
+
     }
 }
