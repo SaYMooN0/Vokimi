ee6d3c704c7a62f71a262a2a6a952c7f73e19d2f
SaYMooN <shokoladnysaymoon@mail.ru>
2024-07-02 23:53:25 +0300

    Implemented TextAndImage answers Started working on the ability to create a conclusion for the test

--- VokimiShared/src/models/dtos/draft_tests/DraftGenericTestDto.cs
+++ VokimiShared/src/models/dtos/draft_tests/DraftGenericTestDto.cs
@@ -1,8 +1,5 @@
-﻿using VokimiShared.src.enums;
-using VokimiShared.src.models.db_classes;
+﻿using VokimiShared.src.models.db_classes;
 using VokimiShared.src.models.db_classes.test_creation;
-using VokimiShared.src.models.dtos.draft_tests;
-using VokimiShared.src.models.dtos.tests;
 
 namespace VokimiShared.src.models.dtos.draft_tests
 {
@@ -12,14 +9,14 @@ namespace VokimiShared.src.models.dtos.draft_tests
         public AppUserId CreatorId { get; init; }
         public DraftTestMainInfoDto MainInfo { get; protected set; }
         public DateTime CreationDate { get; init; }
-        public TestConclusionDto? Conclusion { get; protected set; }
+        public TestConclusionId? ConclusionId { get; protected set; }
         public List<QuestionBriefInfoDto> Questions { get; init; }
         public DraftGenericTestDto(DraftGenericTest test) {
             Id = test.Id;
             CreatorId = test.CreatorId;
             MainInfo = new DraftTestMainInfoDto(test.MainInfo);
             CreationDate = test.CreationDate;
-            Conclusion = test.Conclusion is null ? null : new TestConclusionDto(test.Conclusion);
+            ConclusionId = test.Conclusion is null ? null : test.Conclusion.Id;
             Questions = test.Questions.Select(q => new QuestionBriefInfoDto(q)).ToList();
         }
 
