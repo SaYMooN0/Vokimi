5fc02b1735440a0392f4f15dcc9f13e913dafaa9
SaYMooN <shokoladnysaymoon@mail.ru>
2024-07-13 15:54:15 +0300

    Implemented inputs for the results editing page Refactored vokimi storage service

--- Vokimi/Services/VokimiStorageService.cs
+++ Vokimi/Services/VokimiStorageService.cs
@@ -1,10 +1,6 @@
 ﻿using Amazon.S3;
 using Amazon.S3.Model;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using OneOf;
-using OneOf.Types;
-using System.Collections;
-using Vokimi.src;
 using VokimiShared.src;
 using VokimiShared.src.models.db_classes;
 
@@ -18,115 +14,100 @@ public class VokimiStorageService
         _s3Client = s3Client;
         _bucketName = bucketName;
     }
+    private readonly Err
+       fileUploadingErr = new Err("Failed to upload the file"),
+       serverErr = new Err("Server error. Please try again later")
+       ;
+    private async Task<PutObjectResponse> PutObjectIntoStorage(string objKey, Stream fileStream) {
+        PutObjectRequest putRequest = new() {
+            BucketName = _bucketName,
+            Key = objKey,
+            InputStream = fileStream
+        };
+
+        return await _s3Client.PutObjectAsync(putRequest);
+    }
+
     public async Task<OneOf<string, Err>> SaveDraftTestCover(DraftTestId id, Stream fileStream) {
         try {
             string key = $"{ImgOperationsHelper.DraftTestCoversFolder}/{id}";
-            var putRequest = new PutObjectRequest {
-                BucketName = _bucketName,
-                Key = key,
-                InputStream = fileStream
-            };
-
-            PutObjectResponse response = await _s3Client.PutObjectAsync(putRequest);
+            PutObjectResponse response = await PutObjectIntoStorage(key, fileStream);
 
             if (response.HttpStatusCode == System.Net.HttpStatusCode.OK) {
                 return key;
             }
             else {
-                return new Err("Failed to upload the file");
+                return fileUploadingErr;
             }
-        } catch (Exception ex) {
-            return new Err("Server error. Please try again later");
-        }
+        } catch (Exception ex) { return serverErr; }
     }
     public async Task<OneOf<string, Err>> SaveDraftTestAnswerImage(Stream fileStream, DraftTestQuestionId questionId) {
         try {
             string key = $"{ImgOperationsHelper.DraftTestAnswersFolder}/{questionId}/{Guid.NewGuid()}";
-            var putRequest = new PutObjectRequest {
-                BucketName = _bucketName,
-                Key = key,
-                InputStream = fileStream
-            };
-
-            PutObjectResponse response = await _s3Client.PutObjectAsync(putRequest);
+            PutObjectResponse response = await PutObjectIntoStorage(key, fileStream);
 
             if (response.HttpStatusCode == System.Net.HttpStatusCode.OK) {
                 return key;
             }
             else {
-                return new Err("Failed to upload the file");
+                return fileUploadingErr;
             }
         } catch (Exception ex) {
-            return new Err("Server error. Please try again later");
+            return serverErr;
         }
     }
-    public async Task ClearUnusedAnswerImagesForQuestion(DraftTestQuestionId questionId, IEnumerable<string> reservedKeys) {
-        try {
-            var listRequest = new ListObjectsV2Request {
-                BucketName = _bucketName,
-                Prefix = $"{ImgOperationsHelper.DraftTestAnswersFolder}/{questionId}/"
-            };
-
-            ListObjectsV2Response listResponse;
-            do {
-                listResponse = await _s3Client.ListObjectsV2Async(listRequest);
-                var keysToDelete = listResponse.S3Objects
-                    .Select(o => o.Key)
-                    .Where(key => !reservedKeys.Contains(key))
-                    .ToList();
-
-                if (keysToDelete.Any()) {
-                    var deleteRequest = new DeleteObjectsRequest {
-                        BucketName = _bucketName,
-                        Objects = keysToDelete.Select(key => new KeyVersion { Key = key }).ToList()
-                    };
-
-                    await _s3Client.DeleteObjectsAsync(deleteRequest);
-                }
-
-                listRequest.ContinuationToken = listResponse.NextContinuationToken;
-            } while (listResponse.IsTruncated);
-        } catch (Exception ex) { }
-    }
-    public async Task<OneOf<string, Err>> SaveDraftTestConclusionImage(
-        Stream fileStream, DraftTestId testId, string imgSessionKey) {
+    public async Task<OneOf<string, Err>> SaveDraftTestConclusionImage(Stream fileStream, DraftTestId testId, string imgSessionKey) {
         try {
             string key = $"{ImgOperationsHelper.DraftTestConclusionsFolder}/{testId}/{imgSessionKey}";
-            var putRequest = new PutObjectRequest {
-                BucketName = _bucketName,
-                Key = key,
-                InputStream = fileStream
-            };
-
-            PutObjectResponse response = await _s3Client.PutObjectAsync(putRequest);
+            PutObjectResponse response = await PutObjectIntoStorage(key, fileStream);
 
             if (response.HttpStatusCode == System.Net.HttpStatusCode.OK) {
                 return key;
             }
             else {
-                return new Err("Failed to upload the file");
+                return fileUploadingErr;
             }
-        } catch (Exception ex) {
-            return new Err("Server error. Please try again later");
-        }
+        } catch { return serverErr; }
     }
-    public async Task<Err> ClearDraftTestConclusionUnusedImages(DraftTestId testId, string? usedKey = null) {
+    public async Task<OneOf<string, Err>> SaveDraftTestResultImage(Stream fileStream, DraftTestId testId, string inputKey) {
         try {
+            string key = $"{ImgOperationsHelper.DraftTestResultsFolder}/{testId}/{inputKey}";
+            PutObjectResponse response = await PutObjectIntoStorage(key, fileStream);
 
+            if (response.HttpStatusCode == System.Net.HttpStatusCode.OK) {
+                return key;
+            }
+            else {
+                return fileUploadingErr;
+            }
+        } catch { return serverErr; }
+    }
+    private async Task<Err> ClearUnusedImages(string prefix, IEnumerable<string>? reservedKeys = null) {
+        try {
             var listRequest = new ListObjectsV2Request {
                 BucketName = _bucketName,
-                Prefix = $"{ImgOperationsHelper.DraftTestConclusionsFolder}/{testId}/"
+                Prefix = prefix
             };
 
             ListObjectsV2Response listResponse;
             do {
                 listResponse = await _s3Client.ListObjectsV2Async(listRequest);
-                var objectsToDelete = listResponse.S3Objects
-                    .Where(o => o.Key != usedKey)
-                    .Select(o => new KeyVersion { Key = o.Key })
-                    .ToList();
 
-                if (objectsToDelete.Count > 0) {
+                List<KeyVersion> objectsToDelete;
+
+                if (reservedKeys is null || reservedKeys.Count() == 0) {
+                    objectsToDelete = listResponse.S3Objects
+                        .Select(o => new KeyVersion { Key = o.Key })
+                        .ToList();
+                }
+                else {
+                    objectsToDelete = listResponse.S3Objects
+                        .Where(o => (reservedKeys == null || !reservedKeys.Contains(o.Key)))
+                        .Select(o => new KeyVersion { Key = o.Key })
+                        .ToList();
+                }
+
+                if (objectsToDelete.Any()) {
                     var deleteRequest = new DeleteObjectsRequest {
                         BucketName = _bucketName,
                         Objects = objectsToDelete
@@ -140,15 +121,23 @@ public class VokimiStorageService
                 }
 
                 listRequest.ContinuationToken = listResponse.NextContinuationToken;
-
             } while (listResponse.IsTruncated);
-
-        } catch (Exception ex) {
-            return new Err("Server error. Please try again later");
+        } catch {
+            return serverErr;
         }
         return Err.None;
-
     }
-
-
+    public async Task<Err> ClearDraftTestConclusionUnusedImages(DraftTestId testId, string? usedKey = null) {
+        string prefix = $"{ImgOperationsHelper.DraftTestConclusionsFolder}/{testId}/";
+        IEnumerable<string>? reservedKeys = usedKey is null ? null : [usedKey.ToString()];
+        return await ClearUnusedImages(prefix, reservedKeys);
+    }
+    public async Task ClearUnusedAnswerImagesForQuestion(DraftTestQuestionId questionId, IEnumerable<string> reservedKeys) {
+        string prefix = $"{ImgOperationsHelper.DraftTestAnswersFolder}/{questionId}/";
+        await ClearUnusedImages(prefix, reservedKeys);
+    }
+    public async Task ClearUnusedDraftTestResultsImages(DraftTestId testId, IEnumerable<string> reservedKeys) {
+        string prefix = $"{ImgOperationsHelper.DraftTestResultsFolder}/{testId}/";
+        await ClearUnusedImages(prefix, reservedKeys);
+    }
 }
--- VokimiShared/src/ImgOperationsHelper.cs
+++ VokimiShared/src/ImgOperationsHelper.cs
@@ -8,7 +8,9 @@
             GeneralFolder = "general",
             DraftTestAnswersFolder = "draft_tests_answers",
             DraftTestCoversFolder = "draft_tests_covers",
-            DraftTestConclusionsFolder = "draft_tests_conclusions";
+            DraftTestConclusionsFolder = "draft_tests_conclusions",
+            DraftTestResultsFolder = "draft_tests_results"
+            ;
         public static string DefaultTestCoverImg => $"{GeneralFolder}/test_cover_default.webp";
         public static string ImgUrl(string fileKey) =>
            $"vokimiimgs/GetImage/{fileKey}";
